bdd93d5406d423211ac07f8448b39c31a719372f
Albert G <albertag2002@gmail.com>
2024-06-03 00:55:39 +0530

    txts extracted

--- text_extractor.py
+++ text_extractor.py
@@ -1,24 +1,25 @@
-import requests
 import pandas as pd
+import requests
 from bs4 import BeautifulSoup
 
-def extract_paragraphs(urls):
-    counts = 0
-    for i in urls:
-        counts += 1
-        all_text = ""
-        file_name = "txt_files/output_file" + str(counts) + ".txt"
-        response = requests.get(i)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        target_divs = soup.find_all('div','td-post-content tagdiv-type')
+def parse_url(id, url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    pre_tag = soup.find_all('pre')
+    for p in pre_tag:
+        p.decompose()
+    title = soup.title.text
+    content_box = soup.find('div', attrs={'class': "td-post-content tagdiv-type"})
+    if content_box is None:
+        content_box = soup.find('div', attrs={'class': "td_block_wrap tdb_single_content tdi_130 td-pb-border-top td_block_template_1 td-post-content tagdiv-type"})
+    content = content_box.text if content_box else "Content not found"
 
-        for j in target_divs:
-            for pre in j.find_all('pre'):
-                pre.decompose()
-            all_text += j.get_text(separator='\n', strip=True) + '\n\n'
-        with open(file_name, 'w', encoding='utf-8') as file:
-            file.write(all_text.strip())
+    filename = f"{id}.txt"
+    with open("txt_files/"+filename, 'w', encoding='utf-8') as textfile:
+        textfile.write(f'{title}\n{content}')
+    print(filename + " written successfully")
 
-df = pd.read_excel('problem_statement/Input.xlsx')
+data = pd.read_excel("problem_statement/Input.xlsx")
 
-extract_paragraphs(df['URL'])
+for index, row in data.iterrows():
+    parse_url(row['URL_ID'], row['URL'])
